30f9e3542ea4a6fa5e969db50f0f1a88780d0791
narciso.gomes <narcisoalmeida25@gmail.com>
2024-04-05 14:56:20 -0300

    ADD: validacao lista de tarefas vazia

--- my-projects/react-todo-list/src/components/TaskList.jsx
+++ my-projects/react-todo-list/src/components/TaskList.jsx
@@ -6,26 +6,83 @@ const TaskList = () => {
     const taskInput = document.getElementById("task");
     if (taskInput.value) {
       setTarefa([...tarefas, taskInput.value]);
-      taskInput.value = ''
+      taskInput.value = "";
     }
   }
   return (
     <>
-      <div>
-        <input
-          type="text"
-          placeholder="Informe uma tarefa..."
-          id="task"
-        ></input>{" "}
-        <button onClick={addTask}>+ Adicionar</button>
-      </div>
-
-      <div>
-        <ul>
-          {tarefas.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <div style={{ width: "500px", display: "flex", gap: "10px" }}>
+          <input
+            style={{
+              border: "1px solid #6C63FF",
+              borderRadius: "4px",
+              padding: "10px",
+              flex: "1",
+              color: "#6C63FF",
+            }}
+            type="text"
+            placeholder="Informe uma tarefa..."
+            id="task"
+          ></input>{" "}
+          <button
+            style={{
+              background: "#6C63FF",
+              color: "#fff",
+              border: "none",
+              padding: "10px",
+              cursor: "pointer",
+            }}
+            onClick={addTask}
+          >
+            + Adicionar
+          </button>
+        </div>
+        {tarefas.length > 0 ? (
+          <ul style={{ width: "500px", margin: "0", padding: "0" }}>
+            {tarefas.map((item) => (
+              <ol
+                key={item}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "start",
+                  gap: "5px",
+                  margin: "0",
+                  borderBottom: "1px solid #6C63FF70",
+                  padding: "10px 0px",
+                  fontFamily: "Arial",
+                  color: "#252525",
+                  textTransform: "uppercase",
+                }}
+              >
+                <input type="checkbox" id={item} name={item} />
+                <label htmlFor={item}>{item}</label>
+              </ol>
+            ))}
+          </ul>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              alignItems: "center",
+              fontFamily: "Arial",
+            }}
+          >
+            <img src="./empty.svg" alt="Vazio" />
+            <p>Sem tarefas...</p>
+          </div>
+        )}
       </div>
     </>
   );
