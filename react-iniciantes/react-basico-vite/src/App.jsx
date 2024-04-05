7683e10a1bb155c2fa3f0e08ba468b5327e9943e
narciso.gomes <narcisoalmeida25@gmail.com>
2024-04-04 23:35:32 -0300

    ADD: exercicio

--- react-iniciantes/react-basico-vite/src/App.jsx
+++ react-iniciantes/react-basico-vite/src/App.jsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React from "react";
+
+const luana = {
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
 
 const App = () => {
-   return <div>App React</div>
-}
+  const dados = luana;
+
+  const total = dados.compras
+    .map((item) => +item.preco.replace("R$ ", ""))
+    .reduce((acc, currentValue) => acc + currentValue, 0);
+
+  return (
+    <>
+      <p>
+        <b>Nome: </b> {dados.cliente}
+      </p>
+      <p>
+        <b>Idade: </b> {dados.idade}
+      </p>
+      <p>
+        <b>Situação: </b>{" "}
+        <span style={{ color: dados.ativa ? "green" : "red" }}>
+          {dados.ativa ? "Ativa" : "Inativa"}
+        </span>
+      </p>
+      <p>
+        <b>Total gasto: </b> R$ {total}
+      </p>
+      {total > 10000 && <p>Você está gastando muito.</p>}
+    </>
+  );
+};
 
 export default App;
